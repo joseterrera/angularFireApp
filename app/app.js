6a12e840e7074ff8a0c07e1aea6a1c913be1f207
joseterrera <joseterrera@yahoo.com>
2015-07-31 16:24:14 -0500

    creating the Channels sidebar

--- app/app.js
+++ app/app.js
@@ -9,63 +9,100 @@
  * Main module of the application.
  */
 angular
-    .module('angularfireSlackApp', [
-        'firebase',
-        'angular-md5',
-        'ui.router'
-    ])
-    .config(function($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            .state('home', {
-                url: '/',
-                templateUrl: 'home/home.html'
-            })
-            .state('login', {
-                url: '/login',
-                controller: 'AuthCtrl as authCtrl',
-                templateUrl: 'auth/login.html',
-                resolve: {
-                    requireNoAuth: function($state, Auth) {
-                        return Auth.$requireAuth().then(function(auth) {
-                            $state.go('home');
-                        }, function(error) {
-                            return;
-                        });
-                    }
-                }
-            })
-            .state('register', {
-                url: '/register',
-                controller: 'AuthCtrl as authCtrl',
-                templateUrl: 'auth/register.html',
-                resolve: {
-                    requireNoAuth: function($state, Auth) {
-                        return Auth.$requireAuth().then(function(auth) {
-                            $state.go('home');
-                        }, function(error) {
-                            return;
-                        });
-                    }
-                }
-            })
-            .state('profile', {
-                url: '/profile',
-                controller: 'ProfileCtrl as profileCtrl',
-                templateUrl: 'users/profile.html',
-                 resolve: {
-    auth: function($state, Users, Auth){
-      return Auth.$requireAuth().catch(function(){
-        $state.go('home');
-      });
-    },
-    profile: function(Users, Auth){
-      return Auth.$requireAuth().then(function(auth){
-        return Users.getProfile(auth.uid).$loaded();
-      });
-    }
-  }
+  .module('angularfireSlackApp', [
+    'firebase',
+    'angular-md5',
+    'ui.router'
+  ])
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'home/home.html',
+        resolve: {
+          requireNoAuth: function($state, Auth){
+            return Auth.$requireAuth().then(function(auth){
+              $state.go('channels');
+            }, function(error){
+              return;
             });
+          }
+        }
+      })
+      .state('login', {
+        url: '/login',
+        controller: 'AuthCtrl as authCtrl',
+        templateUrl: 'auth/login.html',
+        resolve: {
+          requireNoAuth: function($state, Auth) {
+            return Auth.$requireAuth().then(function(auth) {
+              $state.go('home');
+            }, function(error) {
+              return;
+            });
+          }
+        }
+      })
+      .state('register', {
+        url: '/register',
+        controller: 'AuthCtrl as authCtrl',
+        templateUrl: 'auth/register.html',
+        resolve: {
+          requireNoAuth: function($state, Auth) {
+            return Auth.$requireAuth().then(function(auth) {
+              $state.go('home');
+            }, function(error) {
+              return;
+            });
+          }
+        }
+      })
+      .state('profile', {
+        url: '/profile',
+        controller: 'ProfileCtrl as profileCtrl',
+        templateUrl: 'users/profile.html',
+        resolve: {
+          auth: function($state, Users, Auth) {
+            return Auth.$requireAuth().catch(function() {
+              $state.go('home');
+            });
+          },
+          profile: function(Users, Auth) {
+            return Auth.$requireAuth().then(function(auth) {
+              return Users.getProfile(auth.uid).$loaded();
+            });
+          }
+        }
+      })
+      .state('channels', {
+        url: '/channels',
+        controller: 'ChannelsCtrl as channelsCtrl',
+        templateUrl: 'channels/index.html',
+        resolve: {
+          channels: function (Channels){
+            return Channels.$loaded();
+          },
+          profile: function ($state, Auth, Users){
+            return Auth.$requireAuth().then(function(auth){
+              return Users.getProfile(auth.uid).$loaded().then(function (profile){
+                if(profile.displayName){
+                  return profile;
+                } else {
+                  $state.go('profile');
+                }
+              });
+            }, function(error){
+              $state.go('home');
+            });
+          }
+        }
+      })
+      .state('channels.create', {
+        url: '/create',
+        templateUrl: 'channels/create.html',
+        controller: 'ChannelsCtrl as channelsctrl'
+      });
 
-        $urlRouterProvider.otherwise('/');
-    })
-    .constant('FirebaseUrl', 'https://joseterrera-angular.firebaseio.com/');
+    $urlRouterProvider.otherwise('/');
+  })
+  .constant('FirebaseUrl', 'https://joseterrera-angular.firebaseio.com/');
